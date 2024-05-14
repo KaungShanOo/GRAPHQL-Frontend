94d5ed848e8391f4df5da117746fff54feb4f2c9
Kaung Shan Oo <kaung669so@prestigein.com>
2024-05-14 12:50:44 +0700

    all mutations

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@ import {useMutation, useQuery} from '@vue/apollo-composable'
 import gql from 'graphql-tag'
 import { computed, ref, watchEffect } from 'vue'
 import { GraphQLClient } from 'graphql-request'
+import { title } from 'process'
 
+// Queries
 const allGames_QUERY = gql`
   query {
     games{
@@ -31,69 +33,93 @@ const graphQLClient = new GraphQLClient(endpoint, {
   headers: {},
 })
 
-async function addGame(title: any, platform: any) {
-  const mutation = gql `
-    mutation addGame($games: AddGameInput!) {
-      AddGameInput(games: $games) {
-          title
-          platform
-
-      }
-    }
-    `
-  const variables = {
-    "title": title,
-    "platform": platform
-  }
-
-  return await graphQLClient.rawRequest(mutation, variables);
-}
-
-async function game_Mutation(gameId: any, title: any, platform: any) {
-  const mutation = gql`
-    mutation deleteGame($id: ID!) {
-      deleteGame(id: $id) {
-        id
-      }
-    },
-    mutation addGame($games: AddGameInput!) {
-      AddGameInput(games: $games) {
-          title
-          platform
-      }
-    },
-    mutation updateGame($id: ID!, $game: UpdateGameInput!) {
-      updateGame(id: $id, game: $game) {
+// Mutations
+const addMutation = gql`
+  mutation ($game: AddGameInput!) {
+      addGame(game: $game) {
         id
         title
         platform
       }
-    }
-    `
-   const variables = {
-    "id": gameId,
-    "title": title,
-    "platform": platform
-   }
-  return await graphQLClient.rawRequest(mutation, variables);
-}
+  } 
+`
+const updateMutation = gql`
+  mutation ($id: ID!, $edits: EditGameInput) {
+      updateGame(id: $id, edits: $edits) {
+        id
+        title
+        platform
+      }
+  } 
+`
 
+const deleteMutation= gql `
+  mutation ($id: ID!){
+    deleteGame(id: $id) {
+      id
+    }
+  }
+`
+
+// fetch datas
 const { result } = useQuery(allGames_QUERY)
 const allGames = computed(() => result.value?.games?? [])
 const allreviews = computed(() => result.value?.reviews?? [])
 const allauthors = computed(() => result.value?.authors ?? [])
+watchEffect(() => {console.log('fetched',allGames)})
 
-watchEffect(() => {
-  console.log('fetched',allGames)
+const { mutate: addGame } = useMutation(addMutation, () => {
+  return {
+    variables: {
+      game: {
+        title: 'valley on mountain',
+        platform: 'ios'
+      }
+    }
+  }
+})
+const { mutate: updateGame } = useMutation(updateMutation, () => {
+  return {
+    variables: {
+      id:2398,
+      edits: {
+        title: 'valley',
+        platform: 'ios'
+      }
+    }
+  }
+})
+
+const { mutate: deleteGame } = useMutation(deleteMutation, () => {
+  return {
+    variables: {
+      id: 3526
+    }
+  }
+})
+
+async function handleAdd() {
+  await addGame()
+  location.reload()
 }
-)
+
+async function handleUpdate() {
+  await updateGame()
+  location.reload()
+}
+
+async function handleDelete() {
+  await deleteGame()
+  location.reload()
+}
 
 </script>
 
 <template>
   <h1>Games</h1>
-  <button @click="deleteGame(4)">Delete</button>
-  <button @click="addGame('PES', ['PC', 'PS4','Mobile'])">Add</button>
+  <button @click="handleDelete()">Delete</button>
+  <button @click="handleAdd()">Add</button>
+  <button @click="handleUpdate()">Update</button>
   #games
   <div v-for="game in allGames" :key="game.id">
     <p class="id">Id: {{ game.id }}</p>
